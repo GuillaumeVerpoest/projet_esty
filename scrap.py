76c60ada991f9f8749ce9c459417aa3127a15a98
GuillaumeVerpoest <guillaume.verpoest@gmail.com>
2021-08-27 18:47:45 +0200

    chnage data

--- scrap.py
+++ scrap.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import requests
 import pandas as pd
 from connect_mongodb import Conn
+import re
 
 
 
@@ -10,47 +11,79 @@ class Scrap_esty():
     def __init__(self) -> None:
         self.today = datetime.now().strftime("%Y-%m-%d")
         self.list_df = []
+        self.dict_bracelet_fr = {"beaded-bracelets":"bracelets-perles",
+                                "bangles":"bracelets-jonc",
+                                "charm-bracelets": "bracelets-breloques",
+                                "woven-and-braided-bracelets":"bracelets-tissés-tressés",
+                                "cuff-bracelets": "Bracelets manchette",
+                                "chain-and-link-bracelets":"Bracelets-chaînes-maillons"}
 
-    def scraping(self, html_text, produit):
+        self.dict_collier_fr = {"pendants":"Pendentifs",
+                                "chokers":"ras-de-cou",
+                                "charm-necklaces":"colliers-breloques",
+                                "crystal-necklaces":"colliers-cristal",
+                                "monogram-and-name-necklaces":"collier-prenom-monogramme",
+                                "beaded-necklaces": "collier-perles",
+                                "chains":"chaines",
+                                "bib-necklaces":"colliers-plastron",
+                                "tassel-necklaces":"collier-ponpon"
+                                }
+
+
+
+
+    def scraping(self, html_text, produit, jewel):
         soup = BeautifulSoup(html_text, 'html.parser')
         all_li = soup.find_all("li", {"class":"wt-list-unstyled"})
         cpt = 0
         for i in all_li:   
             dict_df = {}
-            price = i.find("span",{"class", "currency-value"}).text.replace(",","")
+            try:
+                price = i.find("span",{"class", "currency-value"}).text.replace(",","")
+            except:
+                pass
             lien = i.find("a",href=True)["href"]
             profil = i.find("p",{"class", "wt-text-caption"}).text
             desc = i.find("h3",{"class", "wt-mb-xs-0"}).text.replace("\n","")
 
             dict_df["date"] = self.today
             dict_df["produit"] = produit
+            dict_df["jewel"] = jewel
             dict_df["price"] = price
             dict_df["lien"] = lien
-            dict_df["profil"] = profil
-            dict_df["desc"] = desc
+            dict_df["profil"] = re.sub(' +', ' ', profil)
+            dict_df["desc"] = re.sub(' +', ' ', desc.replace('"',""))
             dict_df["id"] = cpt
             self.list_df.append(dict_df)
             cpt+=1
 
 
     @staticmethod
-    def create_url(produit):
-        url = f'https://www.etsy.com/c/jewelry/bracelets/{produit}?explicit=1&locationQuery=3017382'
+    def create_url(jewel, produit):
+        url = f'https://www.etsy.com/c/jewelry/{jewel}/{produit}?explicit=1&locationQuery=3017382'
         print(url)
         return url
 
     def bracelets(self):
+        jewel = "bracelets"
         for produit in ["beaded-bracelets", "charm-bracelets","woven-and-braided-bracelets","bangles","cuff-bracelets","chain-and-link-bracelets"]:
             print("in process..... ", produit)
-            url = self.create_url(produit)
+            url = self.create_url(jewel, produit)
             html_text = requests.get(url).text
-            self.scraping(html_text, produit)
+            self.scraping(html_text, self.dict_bracelet_fr[produit], jewel)
+        jewel = "necklaces"
+        for produit in ["pendants", "chokers","charm-necklaces","crystal-necklaces","monogram-and-name-necklaces","beaded-necklaces","chains","bib-necklaces","tassel-necklaces"]:
+            print("in process..... ", produit)
+            url = self.create_url(jewel, produit)
+            html_text = requests.get(url).text
+            self.scraping(html_text, self.dict_collier_fr[produit],jewel)
         # push mongodb
         df = pd.json_normalize(self.list_df)
-        import json
-        with open('data.json', 'w') as f:
-            json.dump(self.list_df, f)
+        df.desc.replace(r'\r+|\n+|\t+','', regex=True, inplace=True)
+        df.profil.replace(r'\r+|\n+|\t+','', regex=True, inplace=True)
+
         df.to_csv("test.csv")
+        print(df.shape)
         Conn().insert_many(self.list_df)
     
 
@@ -61,6 +94,4 @@ if __name__ == "__main__":
 
 
  
-
-
 
--- streamlit.py
+++ streamlit.py
@@ -4,18 +4,22 @@ import plotly.express as px
 import pandas as  pd 
 from connect_mongodb import Conn
 import re
+from scrap import Scrap_esty
 
 
 
 
 class Design():
 
-    def create_df_calculed(self):
-        l = []
+    @staticmethod
+    def get_df():
         df = Conn().get_data()
-        df.to_csv("test.csv")
         df.price = pd.to_numeric(df.price)
+        return df
 
+    @staticmethod
+    def create_df_stat(df):
+        l = []
         for y in df.date.unique():
             d = {}
             d["date"] = y
@@ -25,8 +29,8 @@ class Design():
                 d[i+"_min"] = df[df["produit"] == i].price.min()
                 d[i+"_median"] = df[df["produit"] == i].price.median()
             l.append(d)
-        df_calculed = pd.json_normalize(l)
-        return df, df_calculed
+        df = pd.json_normalize(l)
+        return df 
         
     
 
@@ -34,27 +38,52 @@ class Design():
 
     def main(self):
 
-        df_origin ,df_graph = self.create_df_calculed()
-        df_origin = df_origin[df_origin.date == df_origin.date.max()]
-        print(df_origin.shape)
-        #df.sort_values(by=['col1'])
-        menu = ["Graphique"]
+        df = self.get_df()
+        menu = ["Bracelets","Colier"]
         choice = st.sidebar.selectbox("Menu",menu)
-        if choice == "Graphique":
-            st.text('This is some text.')
+
+        if (pd.to_datetime(df.date.max())- pd.to_datetime('today')).days >= 14:
+            Scrap_esty().bracelets()
+        
+        if choice == "Bracelets":
+            df = df[df.jewel == "bracelets"]
+            df_stat = self.create_df_stat(df)
+            st.text(f'Dernière Update {df.date.max()}')
 
 
-            categorie = np.unique([i.split("_")[:-1] for i in df_graph.columns.drop("date")]).tolist()
+            categorie = np.unique([i.split("_")[:-1] for i in df_stat.columns.drop("date")]).tolist()
             cat_selected = st.multiselect('Select countries', categorie)
             if not not cat_selected:
                 columns_selected = []
                 for y in cat_selected:
-                    columns_selected.extend([i for i in df_graph.columns if len(re.findall(y, i)) != 0])
-                fig_line = px.line(df_graph, x="date", y=columns_selected)
-                fig_scatter = px.scatter( df_origin[ df_origin["produit"].isin(cat_selected)], x="price", color="produit")
+                    columns_selected.extend([i for i in df_stat.columns if len(re.findall(y, i)) != 0])
+                fig_line = px.line(df_stat, x="date", y=columns_selected)
+                fig_scatter = px.scatter( df[ df["produit"].isin(cat_selected)], x="price", color="produit")
             else:
-                fig_line = px.line(df_graph, x="date", y=df_graph.columns)
-                fig_scatter = px.scatter( df_origin, x="price", y='produit', color="produit")
+                fig_line = px.line(df_stat, x="date", y=df_stat.columns)
+                fig_scatter = px.scatter( df, x="price", y='produit', color="produit")
+
+            st.plotly_chart(fig_line)
+            st.plotly_chart(fig_scatter)
+
+
+        if choice == "Colier":
+            df = df[df.jewel == "necklaces"]
+            df_stat = self.create_df_stat(df)
+            st.text(f'Dernière Update {df.date.max()}')
+
+
+            categorie = np.unique([i.split("_")[:-1] for i in df_stat.columns.drop("date")]).tolist()
+            cat_selected = st.multiselect('Select countries', categorie)
+            if not not cat_selected:
+                columns_selected = []
+                for y in cat_selected:
+                    columns_selected.extend([i for i in df_stat.columns if len(re.findall(y, i)) != 0])
+                fig_line = px.line(df_stat, x="date", y=columns_selected)
+                fig_scatter = px.scatter( df[ df["produit"].isin(cat_selected)], x="price", color="produit")
+            else:
+                fig_line = px.line(df_stat, x="date", y=df_stat.columns)
+                fig_scatter = px.scatter( df, x="price", y='produit', color="produit")
 
             st.plotly_chart(fig_line)
             st.plotly_chart(fig_scatter)
